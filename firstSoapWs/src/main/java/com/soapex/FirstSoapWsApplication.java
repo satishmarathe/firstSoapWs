1373151264b56729a9bd89ae3cf7ecf1e6d429d9
Satish.Marathe <marathesinpune@gmail.com>
2018-11-10 09:24:06 +0530

    changes in progress for exposing a soap webservice

--- firstSoapWs/src/main/java/com/soapex/FirstSoapWsApplication.java
+++ firstSoapWs/src/main/java/com/soapex/FirstSoapWsApplication.java
@@ -1,48 +1,30 @@
 package com.soapex;
 
+import javax.xml.ws.Endpoint;
+
 import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.transport.servlet.CXFServlet;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class FirstSoapWsApplication {
+public class FirstSoapWsApplication implements CommandLineRunner {
+
+	@Value("${service.port}")
+	private String servicePort;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Endpoint.publish("http://localhost:" + servicePort
+				+ "/service/hello-world", new HelloWorldServiceImpl());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSoapWsApplication.class, args);
 	}
-	
-
-	
-	/**
-	 *
-	 * Defining a bean like this below is causing a exception at startup:
-	 * Parameter 1 of constructor in org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration required a bean of type 
-	 * 'org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPath' that could not be found.
-	-*  Bean method 'dispatcherServletRegistration' not loaded because DispatcherServlet Registration found non dispatcher servlet dispatcherServlet
-		Action:
-		Consider revisiting the entries above or defining a bean of type 'org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPath' in your configuration.
-	 * 
-	
-	@Bean
-    public ServletRegistrationBean dispatcherServlet() {
-        return new ServletRegistrationBean(new CXFServlet(), "/soap-api/*");
-    }
-    
-    **/
-	
-	@Bean
-    public ServletRegistrationBean cxfServletRegistration() {
-        return new ServletRegistrationBean(new CXFServlet(), "/soap-api/*");
-    }
-	
-	
-    @Bean(name=Bus.DEFAULT_BUS_ID)
-    public SpringBus springBus() {      
-        return new SpringBus();
-    }
-    
 }
